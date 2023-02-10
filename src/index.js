b668aab1d3eb5f568671fe9bfbb9c8db66a042b7
niblux <niblux2004@yahoo.co.uk>
2023-02-10 14:58:15 +0000

    feat(app): add initial mobile menu to come in @min-768px

--- src/index.js
+++ src/index.js
@@ -1,5 +1,37 @@
-import './styles/styles.scss';
+import './theme/styles.scss';
 // TODO:  create image/assets folder
 import img from './avatar.jpg';
 
-console.log('JS');
+var innerHam = document.querySelector('.ham-inner');
+
+var hamContainer = document.querySelector('.ham-wrapper');
+
+var mobileMenu = document.querySelector('.mobile-menu');
+
+function animateHamburger() {
+  if (!innerHam.classList.contains('transform-after', 'transform-before')) {
+    innerHam.classList.add('transform-after');
+    innerHam.classList.add('transform-before');
+    innerHam.style.backgroundColor = 'transparent';
+  } else {
+    innerHam.classList.remove('transform-after');
+    innerHam.classList.remove('transform-before');
+    innerHam.style.backgroundColor = '';
+  }
+}
+
+function openCloseMenu() {
+  if (!mobileMenu.classList.contains('open')) {
+    mobileMenu.classList.toggle('open');
+    mobileMenu.classList.remove('close');
+  } else {
+    mobileMenu.classList.remove('open');
+    mobileMenu.classList.toggle('close');
+  }
+}
+
+hamContainer.addEventListener('click', () => {
+  console.log('clickety click');
+  animateHamburger();
+  openCloseMenu();
+});
