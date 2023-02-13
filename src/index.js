1cbb314bbf076d63b823c09798339234a700c515
niblux <niblux2004@yahoo.co.uk>
2023-02-13 22:47:33 +0000

    feat(app): make intial layout responsive, mobile first

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import './theme/styles.scss';
 // TODO:  create image/assets folder
 import img from './avatar.jpg';
+import img2 from './me.jpeg';
+import img3 from './laptop.jpg';
+
+let lastScroll = 0;
+
+const header = document.querySelector('header');
 
 var innerHam = document.querySelector('.ham-inner');
 
@@ -8,15 +14,45 @@ var hamContainer = document.querySelector('.ham-wrapper');
 
 var mobileMenu = document.querySelector('.mobile-menu');
 
+var mainContent = document.getElementById('main');
+
+function scrollingHeader() {
+  window.addEventListener('scroll', () => {
+    const currentScroll = window.pageYOffset;
+    if (currentScroll <= 0) {
+      header.classList.remove('scroll-up');
+      return;
+    }
+    if (
+      currentScroll > lastScroll &&
+      !header.classList.contains('scroll-down')
+    ) {
+      // down
+      header.classList.add('scroll-down');
+      header.classList.remove('scroll-up');
+    } else if (
+      currentScroll < lastScroll &&
+      header.classList.contains('scroll-down')
+    ) {
+      // up
+      header.classList.add('scroll-up');
+      header.classList.remove('scroll-down');
+    }
+    lastScroll = currentScroll;
+  });
+}
+
 function animateHamburger() {
   if (!innerHam.classList.contains('transform-after', 'transform-before')) {
     innerHam.classList.add('transform-after');
     innerHam.classList.add('transform-before');
     innerHam.style.backgroundColor = 'transparent';
+    mainContent.classList.toggle('blur');
   } else {
     innerHam.classList.remove('transform-after');
     innerHam.classList.remove('transform-before');
     innerHam.style.backgroundColor = '';
+    mainContent.classList.toggle('blur');
   }
 }
 
@@ -35,3 +71,5 @@ hamContainer.addEventListener('click', () => {
   animateHamburger();
   openCloseMenu();
 });
+
+scrollingHeader();
